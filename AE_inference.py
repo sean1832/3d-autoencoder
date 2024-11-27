e4d8ac22722076fa8f44f0c4106ff5e8ce116224
Zeke Zhang <sean1832725142@gmail.com>
2024-11-28 06:34:56 +1100

    update on network architecture and input data structure

--- AE_inference.py
+++ AE_inference.py
@@ -3,15 +3,16 @@ from pathlib import Path
 
 import numpy as np
 import torch
+from torch import nn
 
 from vox_encoder import DATA_DIR, OUTPUT_DIR
-from vox_encoder.autoencoder import VoxelAutoencoder
+from vox_encoder.autoencoder import VoxelAutoencoder_1Layers, VoxelAutoencoder_2Layers
 from vox_encoder.data_utils import extract_2d, insert_and_replace_2d
 from vox_encoder.file_io import load_data
 
 
-def load_model(ckpt_path: str | Path, input_dim: int, latent_dim: int) -> VoxelAutoencoder:
-    model = VoxelAutoencoder(input_dim, latent_dim)
+def load_model(ckpt_path: str | Path, input_dim: int, latent_dim: int) -> nn.Module:
+    model = VoxelAutoencoder_1Layers(input_dim, latent_dim)
     checkpoint = torch.load(ckpt_path)
     model.load_state_dict(checkpoint["model_state_dict"])
 
@@ -20,7 +21,7 @@ def load_model(ckpt_path: str | Path, input_dim: int, latent_dim: int) -> VoxelA
     return model
 
 
-def inference(model: VoxelAutoencoder, input_data: torch.Tensor, threshold: float = 0.5):
+def inference(model: nn.Module, input_data: torch.Tensor, threshold: float = 0.5):
     with torch.no_grad():
         # Add batch dimension if it doesn't exist
         if input_data.dim() == 1:
@@ -39,10 +40,12 @@ def inference(model: VoxelAutoencoder, input_data: torch.Tensor, threshold: floa
         return output, thresholded_output
 
 
-def prepare_for_json(tensor: torch.Tensor, original_data: list[list[float]]) -> list:
+def prepare_for_json(
+    tensor: torch.Tensor, original_data: list[list[float]], column_idx: int
+) -> list:
     # Convert tensor to numpy array and then to list
     flat_data = tensor.cpu().numpy().tolist()
-    result = insert_and_replace_2d(original_data, 3, flat_data)
+    result = insert_and_replace_2d(original_data, column_idx, flat_data)
     return result
 
 
@@ -51,9 +54,13 @@ def main():
     output_dir = Path(OUTPUT_DIR, "inference")
 
     # Parameters
-    input_dim = 40392
-    latent_dim = 256
+    input_dim = 8800
+    latent_dim = 16
 
+    # Data
+    data_index = 6  # column of the data that is the state of the voxel
+
+    # Paths
     checkpoint_path = Path(OUTPUT_DIR, "AE_checkpoint.pth")
     original_data_path = Path(data_root, "inference_0")
     output_raw_path = Path(output_dir, "inference_output_raw.json")
@@ -64,7 +71,7 @@ def main():
 
     # Load inference data
     original_data = load_data(original_data_path)
-    clean_data = extract_2d(original_data, 3, float)
+    clean_data = extract_2d(original_data, data_index, float)
 
     # Print debug information
     print(f"Input data type: {type(clean_data)}")
@@ -86,10 +93,10 @@ def main():
     # Save outputs
     try:
         with open(output_raw_path, "w") as f:
-            json.dump(prepare_for_json(raw_output, original_data), f)
+            json.dump(prepare_for_json(raw_output, original_data, data_index), f)
 
         with open(output_thresh_path, "w") as f:
-            json.dump(prepare_for_json(thresholded_output, original_data), f)
+            json.dump(prepare_for_json(thresholded_output, original_data, data_index), f)
 
         print(f"Saved outputs to {output_dir}")
 
--- AE_train.py
+++ AE_train.py
@@ -8,8 +8,9 @@ from torch.utils.data import DataLoader, TensorDataset
 from tqdm import tqdm
 
 from vox_encoder import DATA_RAW_PATH, OUTPUT_DIR
-from vox_encoder.autoencoder import VoxelAutoencoder
-from vox_encoder.data_utils import extract_2d
+from vox_encoder.autoencoder import VoxelAutoencoder_1Layers, VoxelAutoencoder_2Layers
+
+# from vox_encoder.data_utils import extract_2d
 from vox_encoder.file_io import load_data
 from vox_encoder.loss import weighted_binary_cross_entropy
 
@@ -17,7 +18,8 @@ from vox_encoder.loss import weighted_binary_cross_entropy
 def process_file(file_path: str, type: dtype) -> torch.Tensor:
     """Helper function to process a single file."""
     raw_data = load_data(file_path)
-    flat_data = extract_2d(raw_data, 3, float)
+    # flat_data = extract_2d(raw_data, 3, float)
+    flat_data = raw_data
     return torch.tensor(flat_data, dtype=type)
 
 
@@ -42,7 +44,7 @@ def construct_data(path: str, type: dtype) -> list[torch.Tensor]:
 def train_model_1(model, train_loader, num_epochs=50, device: str | torch.device = "cpu"):
     # criterion = nn.BCELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.6)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.8)
 
     model.to(device)  # Move model to the specified device
 
@@ -54,7 +56,9 @@ def train_model_1(model, train_loader, num_epochs=50, device: str | torch.device
             optimizer.zero_grad()
             input = inputs[0].to(device)  # Move inputs to device
             outputs = model(input)
-            loss = weighted_binary_cross_entropy(outputs, input, weights=[0.28, 0.72])
+            loss = weighted_binary_cross_entropy(
+                outputs, input, weights=[0.33, 0.67]
+            )  # sparse voxel [0.28, 0.72]
             loss.backward()
             optimizer.step()
 
@@ -71,34 +75,37 @@ def train_model_1(model, train_loader, num_epochs=50, device: str | torch.device
 
 
 def main():
-    input_dim = 40392  # voxel grid dimension
-    latent_dim = 256
-    num_epoch = 50
+    input_dim = 8800  # voxel grid dimension
+    latent_dim = 16
+    num_epoch = 300
 
     # Determine if a GPU is available and set the device accordingly
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    model = VoxelAutoencoder(input_dim, latent_dim)
+    model = VoxelAutoencoder_1Layers(input_dim, latent_dim)
 
     tensor_datas = construct_data(DATA_RAW_PATH, torch.float32)
     print(f"Loaded {len(tensor_datas)} data files")
     tensor_dataset = TensorDataset(torch.stack(tensor_datas))
     train_loader = DataLoader(tensor_dataset, batch_size=32, shuffle=True)
 
-    model, optimizer, loss, epoch = train_model_1(
-        model, train_loader, num_epochs=num_epoch, device=device
-    )
-
-    torch.save(
-        {
-            "model_state_dict": model.state_dict(),
-            "optimizer_state_dict": optimizer.state_dict(),
-            "epoch": epoch,
-            "loss": loss.item(),
-        },
-        Path(OUTPUT_DIR, "AE_checkpoint.pth"),
-    )
-    print(f"Model saved to {Path(OUTPUT_DIR, 'AE_checkpoint.pth')}")
+    try:
+        model, optimizer, loss, epoch = train_model_1(
+            model, train_loader, num_epochs=num_epoch, device=device
+        )
+    except KeyboardInterrupt:
+        print("Training interrupted by user.")
+    finally:
+        torch.save(
+            {
+                "model_state_dict": model.state_dict(),
+                "optimizer_state_dict": optimizer.state_dict(),
+                "epoch": epoch,
+                "loss": loss.item(),
+            },
+            Path(OUTPUT_DIR, "AE_checkpoint.pth"),
+        )
+        print(f"Model saved to {Path(OUTPUT_DIR, 'AE_checkpoint.pth')}")
 
 
 if __name__ == "__main__":
